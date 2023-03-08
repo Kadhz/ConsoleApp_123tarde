7145c683d4921d0cb7bdb4da7dd32216d1f240c0
Kadhz <carloskadu.silva123@gmail.com>
2023-03-08 14:22:39 -0300

    Definição de variaveis

--- ConsoleApp_123tarde/Produto.cs
+++ ConsoleApp_123tarde/Produto.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp_123tarde
 {
-    internal class Produto
+    public class Produto
     {
+        private int  Id;
+        private string Descricao;
+        private decimal Valor;
+
+
+        public Produto(int id, string descricao, decimal valor ) {
+
+            Id = id;
+            Descricao = descricao;  
+            Valor = valor;  
+        }
     }
 }
