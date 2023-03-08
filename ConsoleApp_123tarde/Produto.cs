d73b8dc625ca7f54e47875319821eeae4c5756b9
Kadhz <carloskadu.silva123@gmail.com>
2023-03-08 15:33:58 -0300

    Criei set para as variaveis

--- ConsoleApp_123tarde/Produto.cs
+++ ConsoleApp_123tarde/Produto.cs
@@ -9,29 +9,50 @@ namespace ConsoleApp_123tarde
 {
     public class Produto
     {
-        private int  Id;
+        private int Id;
         private string Descricao;
         private decimal Valor;
 
 
-        public Produto(int id, string descricao, decimal valor ) {
+        public Produto(int id, string descricao, decimal valor)
+        {
 
             Id = id;
-            Descricao = descricao;  
-            Valor = valor;  
+            Descricao = descricao;
+            Valor = valor;
         }
         public int getId()
         {
             return Id;
         }
+        
         public string getDescricao()
+
         {
             return Descricao;
         }
+        
         public decimal getValor()
+
         {
             return Valor;
         }
+        public void setId(int id)
+        {
+            Id = id;
+        }
+        public void setDescricao(string descricao)
+        {
+            Descricao = descricao;
+        }
+
+
+        public void setValor(decimal valor)
+        {
+            Valor = valor;  
+        }
+
 
     }
 }
+
--- ConsoleApp_123tarde/Program.cs
+++ ConsoleApp_123tarde/Program.cs
@@ -1,7 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp_123tarde;
+
 Console.WriteLine("Hello, World!");
 
 
 
 
 Console.WriteLine("oi");
+Console.WriteLine("Hello World");
+
+
+Produto p1 = new Produto(1,"mouse xing ling", 10.90M);
+Produto p2 = new Produto(2, "Teclado Redragon", 100M);
+
+Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
+
+p1.setValor(30M);
+p1.setDescricao("mouse Razer");
+
+Console.WriteLine(p1.getDescricao()+ " " + p1.getValor());
